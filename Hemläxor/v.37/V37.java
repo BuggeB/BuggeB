3fe0684e1f87eef81643625ab3a489102bf1481b
BuggeB <54797055+BuggeB@users.noreply.github.com>
2019-09-12 15:02:04 +0200

    Steg 3

--- Hemläxor/v.37/V37.java
+++ Hemläxor/v.37/V37.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
 
 public class V37 {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
-Scanner scan = new Scanner(System.in);
-} 
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Whats your first name?");
+		String name = scan.next();
+		
+		System.out.println(name);
+	} 
 }
