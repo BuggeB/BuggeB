1bb2a5ceba4b15cf5dd3201cf3f6dc977b2416c0
BuggeB <54797055+BuggeB@users.noreply.github.com>
2019-10-24 10:28:46 +0200

    Create One.java
    KOmmentarer står i eclipse

---
+++ Hemläxor/v.43/One.java
@@ -0,0 +1,11 @@
+
+public class One {
+	
+	public static double volume(double radius){
+		//Precis under här så förklarar jag för programmet hur man räknar ut volymen på ett klot.
+		double volume = (4*Math.PI *Math.pow(radius, 3)/3);
+		return volume;
+	}
+	
+	
+}
