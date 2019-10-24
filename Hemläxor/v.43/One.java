17fb01ba9f6266259833587c520c199cff69b9bf
BuggeB <54797055+BuggeB@users.noreply.github.com>
2019-10-24 10:31:01 +0200

    Update One.java
    Beskrivning finns i eclipse

--- Hemläxor/v.43/One.java
+++ Hemläxor/v.43/One.java
@@ -1,6 +1,10 @@
 
 public class One {
-	
+	//Skapar en main string
+		public static void main(String[] args) {
+			//Under här skriver jag Sysout för att kunna få fram ett svar.
+			System.out.println(volume(10)+"Liter");}
+		
 	public static double volume(double radius){
 		//Precis under här så förklarar jag för programmet hur man räknar ut volymen på ett klot.
 		double volume = (4*Math.PI *Math.pow(radius, 3)/3);
