ab359934d700f15556146c3528958af5849692dd
BuggeB <54797055+BuggeB@users.noreply.github.com>
2019-11-11 10:41:43 +0100

    Andra metoden klar (Java dokumentering gör jag senare)

--- Hemläxor/Inlämning/Metoder.java
+++ Hemläxor/Inlämning/Metoder.java
@@ -3,9 +3,15 @@ public class Metoder {
 
 	public static void main(String[] args) {
 		System.out.println(farenheitToCelcius(50.0));
-	}
+		System.out.println(kelvinToCelcius(0));
+	} 
 	public static double farenheitToCelcius (double farenheit) {
 		double celcius = ((farenheit - 32)*5)/9;
 		return celcius;
 	}
+	public static double kelvinToCelcius (double kelvin) {
+		double celcius = (kelvin-273.15);
+		return celcius;
+	}
 }
+
