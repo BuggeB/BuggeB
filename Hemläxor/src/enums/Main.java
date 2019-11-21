6634c9976756496b702a43b1b207e1b21d40c40a
BuggeB <54797055+BuggeB@users.noreply.github.com>
2019-11-21 10:01:52 +0100

    NU HAR JAG GJORT UPPGIFT 3, dokumentation kommer senare

--- Hemläxor/src/enums/Main.java
+++ Hemläxor/src/enums/Main.java
@@ -1,6 +1,13 @@
 package enums;
 
 public class Main {
+	
+	static double g_swe = 9.82;
+	static double G = 6.67408*Math.pow(10, -11);
+	static double R = 8.3144621;
+	static double p_0 = 1000;
+	static double c = 299792458;
+	
 
 	public static void main(String[] args) {
 		System.out.println(farenheitToCelcius(50.0));
@@ -14,6 +21,10 @@ public class Main {
 		double celcius = (kelvin-273.15);
 		return celcius;
 	}
-	public static double fluidpressure
+	public static double fluidPressure(FluidTable fluid, double deep) {
+		double Pressure = 0;
+		Pressure = fluid.density*g_swe*deep;
+		return Pressure;
+	}
 }
 
