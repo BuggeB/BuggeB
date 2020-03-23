5c5ec5d7a5b3643daa6a6c29bd140a766e20fda8
BuggeB <54797055+BuggeB@users.noreply.github.com>
2020-03-23 17:04:18 +0100

    Första commiten
    Lagt in alla globala värden

---
+++ UppgitTalspelet.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class UppgitTalspelet {
+	static Scanner input = new Scanner (System.in);
+	static Random rand = new Random();
+	static String playername;
+	static int playerGuess;
+	static int EasyMediumHard;
+	static int RightAnswer;
+	static int Hints = 0;
+	static boolean hard =false;
+public static void main(String[] args) {
+	
+}
+}
