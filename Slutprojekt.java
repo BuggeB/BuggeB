1ba949a27b4ef1d0bf08049489d957b7810fb979
BuggeB <54797055+BuggeB@users.noreply.github.com>
2020-05-11 10:57:46 +0200

    Commit 2
    Nu så har jag faktiskt använt mig av exception trots att jag inte hade med det i min planering, men det är pågrund av att om jag hade skrivit in en bokstav när jag skulle väla svårighetsgrad så hade konsolen kraschat

--- Slutprojekt.java
+++ Slutprojekt.java
@@ -4,16 +4,40 @@ public class Slutprojekt {
 
 	static Scanner input = new Scanner(System.in);
 	static String playername;
-	
-	
+	static int difficulties;
+
 	public static void main(String[] args) {
-		
+		Spelet();
 
 	}
+
 	public static void Spelet () {
 		System.out.println("Välkommen till Hänga Gubbe!");
 		regler();
 		System.out.println("Vad heter du?");
 		String playername = input.nextLine();
+		while (true) {
+			System.out.println("Välj svårighetsgrad: \n 1 (Lätt) \n 2 (Svår)");
+			difficulties = Exception();
+		}
+	}
+
+	public static void regler() {
+		System.out.println(
+				"Spelets regler: \n Välj svårighetsgrad, klicka 1 för lättare grad, klicka 2 för lite utmaning \n Gissa på ordet \n Gissar du rätt på en bokstav kommer den komma upp i ordet \n Gissar du rätt på hela ordet vinner du \n Lycka till!");
+	}
+
+	public static int Exception() {
+		String WrongFromUser = "";
+		while (true) {
+			WrongFromUser = input.nextLine();
+			try {
+				return Integer.parseInt(WrongFromUser);
+
+			} catch (Exception e) {
+				System.out.println("Fel input! Du måste skriva 1 eller 2!");
+				input.hasNext();
+			}
+		}
 	}
 }
