866f7f7eeeb44f20c3192b320d21503f1dfe4b7d
BuggeB <54797055+BuggeB@users.noreply.github.com>
2020-05-11 17:56:27 +0200

    Commit 6
    Nu har jag lag till en funktion för att starta om spelet om man vill, samt att jag har snyggat till textmeddelanden

--- Slutprojekt.java
+++ Slutprojekt.java
@@ -9,41 +9,45 @@ public class Slutprojekt {
 	static String userInput;
 	static int difficulties;
 	static int playerGuess = 0;
-	static String[] easyWords = {"Hej"};
-	static String[] hardWords = {"Banan"};
+	static String[] easyWords = { "Hej" };
+	static String[] hardWords = { "Banan" };
 	static String word;
 	static String unknown;
 	static List<String> wrongGuess = new ArrayList<String>();
-	
+	static boolean restart = true;
+
 	public static void main(String[] args) {
 		Spelet();
 
 	}
 
-	public static void Spelet () {
+	public static void Spelet() {
 		System.out.println("Välkommen till Hänga Gubbe!");
 		regler();
 		System.out.println("Vad heter du?");
 		String userInput = input.nextLine();
+		while(restart) {
+			
+		
 		chooseDifficulty();
-		while (playerGuess < 10) {
+		while (playerGuess < 10 && unknown.contains("_")) {
 			System.out.println("Gissa på en bokstav");
 			String guess = input.next();
 			hang(guess);
 		}
-		
+		}
 	}
-	
+
 	public static void hang(String guess) {
 		String newUnknown = "";
-		for (int i = 0; i<word.length(); i++) {
-			if(word.charAt(i) == guess.charAt(0)) {
-				newUnknown += guess.charAt(0);
-			}
-			else if (unknown.charAt(i)!="_".charAt(0)) {
+		for (int i = 0; i < word.length(); i++) {
+			char wordChar = Character.toLowerCase(word.charAt(i));
+			char guessChar = Character.toLowerCase(guess.charAt(0));
+			if (wordChar == guessChar) {
+				newUnknown += guessChar;
+			} else if (unknown.charAt(i) != "_".charAt(0)) {
 				newUnknown += unknown.charAt(i);
-			}
-			else {
+			} else {
 				newUnknown += "_";
 			}
 		}
@@ -52,27 +56,36 @@ public class Slutprojekt {
 			wrongGuess.add(guess);
 			System.out.println(Arrays.toString(wrongGuess.toArray()));
 			System.out.println("Fel gissning!");
-		}
-		else {
+		} else {
 			unknown = newUnknown;
 			System.out.println("Rätt gissning!");
+			System.out.println(unknown);
 		}
 		if (unknown.equalsIgnoreCase(word)) {
 			System.out.println("Grattis du vann!");
+			System.out.println("Om du vill spela igen klicka 1, om du vill avsluta klicka 2");
+			int answer = input.nextInt();
+			if (answer == 2) {
+				restart = false;
+				System.out.println("Tack för att du spelade!");
+			}
 		}
 	}
-	
+
 	public static void chooseDifficulty() {
-			System.out.println("Välj svårighetsgrad: \n 1 (Lätt) \n 2 (Svår)");
-			difficulties = input.nextInt();
-		if(difficulties == 1) {
-			word = easyWords [(int) (Math.random() * easyWords.length)];
-		}
-		else {
-			word = easyWords [(int) (Math.random() * hardWords.length)];
+		System.out.println("Välj svårighetsgrad: \n 1 (Lätt) \n 2 (Svår)");
+		difficulties = input.nextInt();
+		if (difficulties == 1) {
+			word = easyWords[(int) (Math.random() * easyWords.length)];
+		} else {
+			word = hardWords[(int) (Math.random() * hardWords.length)];
 		}
 		unknown = new String(new char[word.length()]).replace("\0", "_");
-		System.out.println(word+" "+unknown);
+		for (char ch : unknown.toCharArray()) {
+			System.out.print(ch);
+			System.out.print(" ");
+		}
+		System.out.println();
 	}
 
 	public static void regler() {
